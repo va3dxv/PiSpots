b2507d70815d473a097b4ecde118aee41105c748
va3dxv <45865432+va3dxv@users.noreply.github.com>
2019-01-05 21:03:42 -0500

    Update pispots.py

--- pispots.py
+++ pispots.py
@@ -53,9 +53,9 @@ def main():
     args = parser.parse_args()
 
     if not any([args.fort, args.vhf, args.ten, args.at, args.twnt]):
-    print(
-        "This application displays DX spots for a specified band on your 16x2 "
-        "Raspberry Pi LCD. Arguments are required but no argument given. Try 'pispots.py -h'")
+        print(
+            "This application displays DX spots for a specified band on your 16x2 "
+            "Raspberry Pi LCD. Arguments are required but no argument given."
 
     elif args.vhf:
 
